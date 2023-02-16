f32db4448f4e6709367e73ded8415a9b63ef4119
Vlad Paulechka <vlad9.ru@gmail.com>
2023-02-16 13:41:30 +0300

    delete column Transaction

--- schemas/userShemas.py
+++ schemas/userShemas.py
@@ -5,7 +5,6 @@ from pydantic import BaseModel, Field
 class UserSchema(BaseModel):
     user_id: int = Field(ge=1)
     date_created:  datetime = Field(default=datetime.now())
-    transactions: int = Field(default=0)
 
 
 class UserInDBSchema(UserSchema):
