15d23f1084c4c97ee7082bbaae60f743c36443e6
Vlad Paulechka <vlad9.ru@gmail.com>
2023-02-22 11:40:05 +0300

    import referral schemas

--- schemas/__init__.py
+++ schemas/__init__.py
@@ -1,3 +1,4 @@
 from .config import ConfigSchema
 from .userShemas import UserSchema, UserInDBSchema
 from .transactionsSchemas import TransactionSchema, TransactionInDBSchema
+from .referralShemas import ReferralInDBSchema, ReferralSchema
